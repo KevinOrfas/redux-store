e5f95bd38609a251625da9c00d55a60d39d65e47
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2018-04-29 14:10:28 +0100

    Set up

---
+++ src/store/reducers.ts
@@ -0,0 +1,28 @@
+import * as fromActions from './actions';
+
+export const initilState = {
+  loaded: false,
+  loading: false,
+  data: [{ label: 'Code something', complete: false }]
+};
+
+export function reducer(state = initilState, action: { type: string; payload: any }) {
+  switch (action.type) {
+    case fromActions.ADD_TODO: {
+      const todo = action.payload;
+      const data = [...state.data, todo];
+      return {
+        ...state,
+        data
+      };
+    }
+    case fromActions.REMOVE_TODO: {
+      const data = state.data.filter(todo => todo.label !== action.payload.label);
+      return {
+        ...state,
+        data
+      };
+    }
+  }
+  return state;
+}
